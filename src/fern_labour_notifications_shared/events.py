74ce62361a67c5c6ac92e963cf7a2fd758b58682
Kieran Gray <34507342+kieran-gray@users.noreply.github.com>
2025-05-06 13:07:13 +0100

    Use fern-labour-core package (#1)
    * Use fern-labour-core package
    
    * update workflows for private package install

--- src/fern_labour_notifications_shared/events.py
+++ src/fern_labour_notifications_shared/events.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 from typing import Any, Self
 
-from fern_labour_notifications_shared.base_event import BaseEvent
+from fern_labour_core.events.event import DomainEvent
 
 
 @dataclass
-class NotificationRequested(BaseEvent):
+class NotificationRequested(DomainEvent):
     @classmethod
     def create(cls, data: dict[str, Any], event_type: str = "notification.requested") -> Self:
         return super().create(event_type=event_type, data=data)
